6d2f5c6e463639f008f2e8ec68593523cb9be35e
Aluno <Aluno@3NXS314>
2025-06-02 19:38:36 -0300

    Add project files.

---
+++ Trabalho de Programação - Exercício 01/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+    class Programa
+{
+    private static double largura;
+
+    static void Main()
+    {
+        Console.WriteLine("Digite a largura do terreno (em metros):");
+        Console.WriteLine("Dgite o comprimento do terreno (em metros):");
+        double comprimento = Convert.ToDouble(Console.ReadLine());
+
+        double area = largura * comprimento;
+
+        Console.WriteLine("A área dp terreno é: " + area + "metros quadrados.");
+    }
+}
